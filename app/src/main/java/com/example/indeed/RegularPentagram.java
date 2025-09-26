d3bbd4e47669306a4423173460dade4231136a16
Xan17 <xan@littleloon.ca>
2025-09-26 13:38:41 -0600

    Created RegularPentagram

---
+++ app/src/main/java/com/example/indeed/RegularPentagram.java
@@ -0,0 +1,10 @@
+package com.example.indeed;
+
+public class RegularPentagram extends Shape{
+    private int side_length;
+    public RegularPentagram(int x, int y, int side_length){
+        this.x = x;
+        this.y = y;
+        this.side_length = side_length;
+    }
+}
